4f9c7b29eb5992d4126d46c4b580cb2cb25d9ec9
SydNS <40373744+SydNS@users.noreply.github.com>
2021-04-10 04:04:28 -0700

    fetching images from the internet is quite easy with flutter as there are specific widgets for it

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+
 //at every end of a widget add a comma
 void main() {
   runApp(MaterialApp(
-      home: Scaffold(
+    home: Scaffold(
+
+        backgroundColor: Colors.blueGrey[900],
         appBar: AppBar(
-          title: Text("data"),
-        ),
+    title: Text("data"),
+    backgroundColor: Colors.blueGrey[900],
+  ),
+      body:Center(
+        child:  Image.network("https://film-book.com/wp-content/uploads/2021/02/greys-anatomy-season-seventeen-tv-show-poster-01-700x400-1.jpg"),
       )
+  )
 
   ));
 }
