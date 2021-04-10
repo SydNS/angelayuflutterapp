e8bb130e37d379dbe3a62fee3af761111e2257f8
SydNS <40373744+SydNS@users.noreply.github.com>
2021-04-10 03:29:53 -0700

    working with the material themes in flutter and scaffold to hold the various wigets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
-
+//at every end of a widget add a comma
 void main() {
-  runApp(MaterialApp(home: Center(child: Text("Syd"),),));
+  runApp(MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("data"),
+        ),
+      )
+
+  ));
 }
