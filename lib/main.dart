a24f62a776685526c365033aa0079d23aa52d44b
SydNS <40373744+SydNS@users.noreply.github.com>
2021-04-10 04:46:45 -0700

    put a text under the image in the column wrapping widget

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,17 @@ void main() {
              ),
            ),
           Center(
-            child:  Image.network("https://film-book.com/wp-content/uploads/2021/02/greys-anatomy-season-seventeen-tv-show-poster-01-700x400-1.jpg"),
-            // child:  Image.network(),
+            // child:  Image.network("https://film-book.com/wp-content/uploads/2021/02/greys-anatomy-season-seventeen-tv-show-poster-01-700x400-1.jpg"),
+            child:  Column(
+              children: [
+                Image.asset("images/greys1.jpeg"),
+                Text("There's an end to every storm. Once all the trees have been uprooted. Once all the houses have been ripped apart. The wind will hush, the clouds will part, the rain will stop, the sky will clear in an instant. But only then, in those quiet moments after the storm, do we learn who was strong enough to survive it.",
+                  style: TextStyle(fontSize: 20,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       )
