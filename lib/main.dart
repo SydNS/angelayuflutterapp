610cf92ad25e436bbfcd24c75c32c3eda72e71d3
SydNS <40373744+SydNS@users.noreply.github.com>
2021-04-10 04:24:13 -0700

    added some text

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,10 @@ void main() {
   ),
       body:Column(
         children: [
-           Text("data",
-
-
+           Text("Greys Anatomy One of few greatest series",
              style: TextStyle(fontSize: 20,
-             color: Colors.white),
+             color: Colors.white,
+             ),
            ),
           Center(
             child:  Image.network("https://film-book.com/wp-content/uploads/2021/02/greys-anatomy-season-seventeen-tv-show-poster-01-700x400-1.jpg"),
